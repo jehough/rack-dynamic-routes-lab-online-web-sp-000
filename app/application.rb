9c72142651ae4f718e7fdd25988e970c5f56bacd
Jim Hough <teachlife@outlook.com>
2019-06-05 18:07:39 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,8 +2,16 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-    if 2+2 == 5
-      item = req.path.split(/items/).last
+    if req.path== '/items'
+      item = req.path.split("/items/").last
+      if Item.all.detect {|obj| obj.name == item}
+        price = Item.all.detect {|obj| obj.name == item}.price
+        resp.write "#{item} cost #{price}"
+        resp.status = 200
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
     else
       resp.write  "Route not found"
       resp.status = 404
