4ffba1b1a7af62c826b4e7f391d53b0b136caa33
Jim Hough <teachlife@outlook.com>
2019-06-05 18:09:05 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-    if req.path== '/items/Figs'
+    if req.path.include('/items/')
       item = req.path.split("/items/").last
       if Item.all.detect {|obj| obj.name == item}
         price = Item.all.detect {|obj| obj.name == item}.price
